f1be65abe871c4d23bf8d55678ea105cc79d2c35
Natalie Salomons Frick <natalie.salomons@gmail.com>
2024-10-04 19:48:41 +0200

    update urlGET

--- src/stores/urlGet.js
+++ src/stores/urlGet.js
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 export const useUrlGet = defineStore('urlGet', {
     state: () => ({
         urlGet: "https://miun-sampi-database-96bc66384ce1.herokuapp.com/api",
-        urlGet_no: "http://localhost:8000/api"
+        // urlGet: "http://localhost:8000/api"
     })
 })
